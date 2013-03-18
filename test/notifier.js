2f8cf18399ea6d2debf2e4a0c93c2a65fc055d35
Cory Virok <coryvirok@gmail.com>
2013-03-18 11:33:48 -0700

    test infinite recursion error and return the last API response

--- test/notifier.js
+++ test/notifier.js
@@ -2,16 +2,40 @@ var assert = require('assert');
 var vows = require('vows');
 
 var notifier = require('../lib/notifier');
+var rollbar = require('../rollbar');
+
+var ACCESS_TOKEN = '8802be7c990a4922beadaaefb6e0327b';
+
+rollbar.init(ACCESS_TOKEN, {environment: 'playground', handler: 'inline'});
 
 var suite = vows.describe('notifier').addBatch({
   'handleError with an Error that has a missing stack': {
     topic: function() {
-      var e = new Error();
+      var e = new Error('test error');
       e.stack = undefined;
       notifier.handleError(e, this.callback);
     },
-    'verify the an error is returned': function(err) {
-      assert.isNotNull(err);
+    'verify no error is returned': function(err, resp) {
+      assert.isNull(err);
+      assert.isObject(resp);
+      assert.include(resp, 'ids');
+    }
+  },
+  'handleError with an infinite recursion stack limit reached Error': {
+    topic: function() {
+      var genError = function(counter) {
+        return genError(++counter);
+      };
+      try {
+        genError();
+      } catch (e) {
+        notifier.handleError(e, this.callback);
+      }
+    },
+    'verify the error was sent': function(err, resp) {
+      assert.isNull(err);
+      assert.isObject(resp);
+      assert.include(resp, 'ids');
     }
   },
   'reportMessage with invalid request object': {
