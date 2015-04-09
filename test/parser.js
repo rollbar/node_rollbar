856e72252b99f7223b3a2d5404b1f83359ab7789
Cory Virok <coryvirok@gmail.com>
2015-04-09 14:18:42 -0700

    Refactored lib/parser to use async library instead of our ghetto attempts at doing similar control-flow logic ourselves. Started getting lib/parser.js and test/parser.js into jslint compliance. All tests pass. @jondeandres

--- test/parser.js
+++ test/parser.js
@@ -11,7 +11,7 @@ var suite = vows.describe('parser').addBatch({
       readJadeFixture(__dirname + '/../fixture/jadeerr.jade',
         false, {breakme: true}, this.callback);
     },
-    'parse the exception with parseException': parseAndVerifyException(false),
+    'parse the exception with parseException': parseAndVerifyException(false)
   },
   'Read in a jade template with an error, compile with a filename': {
     topic: function() {
@@ -79,46 +79,46 @@ var suite = vows.describe('parser').addBatch({
   },
   'Multiple errors': {
     'Simple': {
-      topic: function(err) {
+      topic: function (err) {
         var exc1 = new Error('First error');
         var exc2 = new Error('Second error');
         var containerExc = new Error();
         containerExc.errors = [exc1, exc2];
         return parser.parseException(containerExc, this.callback);
       },
-      'it uses the first error': function(err, parsedObj) {
+      'it uses the first error': function (err, parsedObj) {
         assert.equal(parsedObj.message, 'First error');
       }
     },
     'exception.errors is an object': {
-      topic: function(err) {
+      topic: function (err) {
         var exc1 = new Error('First error');
         var exc2 = new Error('Second error');
         var containerExc = new Error();
         containerExc.errors = {first: exc1, second: exc2};
         return parser.parseException(containerExc, this.callback);
       },
-      'one of the errors was used': function(err, parsedObj) {
+      'one of the errors was used': function (err, parsedObj) {
         assert.isTrue(parsedObj.message === 'First error' || parsedObj.message === 'Second error');
       }
     },
     'exception.errors is null': {
-      topic: function(err) {
+      topic: function (err) {
         var containerExc = new Error('Container error');
         containerExc.errors = null;
         return parser.parseException(containerExc, this.callback);
       },
-      'the container error was used': function(err, parsedObj) {
+      'the container error was used': function (err, parsedObj) {
         assert.equal(parsedObj.message, 'Container error');
       }
     },
     'exception.errors is an empty array': {
-      topic: function(err) {
+      topic: function (err) {
         var containerExc = new Error('Container error');
         containerExc.errors = [];
         return parser.parseException(containerExc, this.callback);
       },
-      'the container error was used': function(err, parsedObj) {
+      'the container error was used': function (err, parsedObj) {
         assert.equal(parsedObj.message, 'Container error');
       }
     }
