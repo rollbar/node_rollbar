09565e8cd73da34a8f90ecc8f3fcbbd3067584e5
Cory Virok <coryvirok@gmail.com>
2015-04-09 18:37:14 -0700

    Removing outdated middleware. The Express/Connect middleware is implemented and documented in rollbar.js errorHandler()

--- test/json.js
+++ test/json.js
@@ -14,40 +14,46 @@ rollbar.init(ACCESS_TOKEN, {environment: 'playground', handler: 'inline'});
 
 var suite = vows.describe('json').addBatch({
   'should handle circular object references': {
-    topic: function() {
-      var c = {};
-      var circularObject = {c: c};
+    topic: function () {
+      var c, circularObject;
+
+      c = {};
+      circularObject = {c: c};
       c.d = circularObject;
 
       notifier.reportMessageWithPayloadData('test', circularObject, null, this.callback);
     },
-    'verify there were no errors reporting the circular object': function(err, resp) {
+    'verify there were no errors reporting the circular object': function (err, resp) {
       assert.isNull(err);
       assert.isObject(resp);
       assert.include(resp, 'ids');
     }
   },
   'should handle sibling keys that refer to the same object': {
-    topic: function() {
-      var obj = {foo: 'bar'};
-      var testObj = {a: obj, b: obj};
+    topic: function () {
+      var obj, testObj;
+
+      obj = {foo: 'bar'};
+      testObj = {a: obj, b: obj};
 
       notifier.reportMessageWithPayloadData('test', testObj, null, this.callback);
     },
-    'verify there were no errors reporting the object': function(err, resp) {
+    'verify there were no errors reporting the object': function (err, resp) {
       assert.isNull(err);
       assert.isObject(resp);
       assert.include(resp, 'ids');
     }
   },
   'should be able to send unicode characters properly': {
-    topic: function() {
-      var obj = {foo: '☀ ☁ ☂ ☃ ☄ ★ ☆ ☇ ☈ ☉ ☊ ☋ ☌ ☍ ☎ ☏ ☐ ☑ ☒ ☓ ☚ ☛ ☜'};
-      var testObj = {a: obj, b: obj};
+    topic: function () {
+      var obj, testObj;
+
+      obj = {foo: '☀ ☁ ☂ ☃ ☄ ★ ☆ ☇ ☈ ☉ ☊ ☋ ☌ ☍ ☎ ☏ ☐ ☑ ☒ ☓ ☚ ☛ ☜'};
+      testObj = {a: obj, b: obj};
 
       notifier.reportMessageWithPayloadData('test', testObj, null, this.callback);
     },
-    'verify there were no errors reporting the object': function(err, resp) {
+    'verify there were no errors reporting the object': function (err, resp) {
       assert.isNull(err);
       assert.isObject(resp);
       assert.include(resp, 'ids');
--- test/parser.js
+++ test/parser.js
@@ -1,4 +1,4 @@
-/*jslint devel: true, nomen: true, plusplus: true, regexp: true, indent: 2, maxlen: 100 */
+/*jslint devel: true, nomen: true, plusplus: true, regexp: true, unparam: true, indent: 2, maxlen: 100 */
 
 "use strict";
 
@@ -9,32 +9,121 @@ var vows = require('vows');
 
 var parser = require('../lib/parser');
 
+
+/*
+ * Internal
+ */
+
+
+function readJadeFixture(filename, includeJadeFilename, jadeLocals, callback) {
+  fs.readFile(filename, function (err, data) {
+    var opts, jadeFn;
+
+    opts = {};
+
+    if (err) {
+      return callback(err);
+    }
+
+    if (includeJadeFilename) {
+      opts.filename = __dirname + '/../fixture/jadeerr.jade';
+    }
+
+    jadeFn = jade.compile(data, opts);
+    try {
+      jadeFn(jadeLocals);
+      return callback(new Error('expected this to break'));
+    } catch (e) {
+      return callback(null, e);
+    }
+  });
+}
+
+
+function parseAndVerifyException(jadeDebug) {
+  return {
+    topic: function (err, exc) {
+      parser.parseException(exc, this.callback);
+    },
+    'verify stack frames': function (err, parsedObj) {
+
+      var frames, i, numFrames, cur, jadeDebugFound;
+
+      assert.isNull(err);
+      assert.isObject(parsedObj);
+
+      frames = parsedObj.frames;
+      assert.isArray(frames);
+
+      assert.equal(parsedObj.class, 'ReferenceError');
+      assert.equal(parsedObj.message, 'foo is not defined');
+
+      numFrames = frames.length;
+      assert.isTrue(numFrames >= 1);
+
+      jadeDebugFound = false;
+      for (i = 0; i < numFrames; ++i) {
+        cur = frames[i];
+        assert.include(cur, 'method');
+        assert.include(cur, 'filename');
+        assert.include(cur, 'lineno');
+        if (cur.method !== '<jade>') {
+          assert.include(cur, 'colno');
+          assert.isNumber(cur.colno);
+        } else {
+          jadeDebugFound = true;
+        }
+
+        assert.isNumber(cur.lineno);
+        assert.isString(cur.method);
+        assert.isString(cur.filename);
+      }
+
+      if (jadeDebug) {
+        if (!jadeDebugFound) {
+          assert.isFalse('jade debug not found');
+        }
+      } else {
+        if (jadeDebugFound) {
+          assert.isFalse('jade debug found but should not be');
+        }
+      }
+    }
+  };
+}
+
+
+/*
+ * Tests
+ */
+
+
 var suite = vows.describe('parser').addBatch({
   'Read in a jade template with an error, compile with no filename': {
-    topic: function() {
+    topic: function () {
       readJadeFixture(__dirname + '/../fixture/jadeerr.jade',
         false, {breakme: true}, this.callback);
     },
     'parse the exception with parseException': parseAndVerifyException(false)
   },
   'Read in a jade template with an error, compile with a filename': {
-    topic: function() {
+    topic: function () {
       readJadeFixture(__dirname + '/../fixture/jadeerr.jade',
         true, {breakme: true}, this.callback);
     },
     'parse the exception with parseException': parseAndVerifyException(true)
   },
   'Create a new Error in this file': {
-    topic: function() {
+    topic: function () {
       // NOTE: Don't change this next line of code since we verify the context line
       // in the parsed exception below.
       this.callback(null, new Error('Hello World'));
     },
     'parse it with parseException': {
-      topic: function(err, exc) {
+      topic: function (err, exc) {
         return parser.parseException(exc, this.callback);
       },
-      'verify the filename': function(err, parsedObj) {
+      'verify the filename': function (err, parsedObj) {
         assert.isNull(err);
         assert.isObject(parsedObj);
         assert.isArray(parsedObj.frames);
@@ -43,7 +132,7 @@ var suite = vows.describe('parser').addBatch({
         assert.isObject(lastFrame);
         assert.equal(lastFrame.filename, __filename);
       },
-      'verify the context line': function(err, parsedObj) {
+      'verify the context line': function (err, parsedObj) {
         var lastFrame = parsedObj.frames[parsedObj.frames.length - 1];
         assert.isString(lastFrame.code);
         assert.includes(lastFrame.code, 'new Error(\'Hello World\')');
@@ -51,13 +140,13 @@ var suite = vows.describe('parser').addBatch({
     }
   },
   'An error reading a file': {
-    topic: function(err) {
+    topic: function (err) {
       var exc = new Error();
       exc.stack = "Error\n at REPLServer.self.eval (/tmp/file-does-not-exist.js:1:2)" +
         "\n at REPLServer.self.eval (/tmp/other-file-does-not-exist.js:3:4)";
       return parser.parseException(exc, this.callback);
     },
-    'it returns frames without context': function(err, parsedObj) {
+    'it returns frames without context': function (err, parsedObj) {
       assert.equal(parsedObj.frames.length, 2);
       assert.equal(parsedObj.frames[0].filename, "/tmp/other-file-does-not-exist.js");
       assert.equal(parsedObj.frames[0].lineno, 3);
@@ -68,12 +157,13 @@ var suite = vows.describe('parser').addBatch({
     }
   },
   'A coffee script stacktrace': {
-    topic: function(err) {
+    topic: function (err) {
       var exc = new Error();
-      exc.stack = "TypeError: Cannot read property 'foo' of undefined\n at example (/tmp/example.coffee:2:3, <js>:5:20)";
+      exc.stack = "TypeError: Cannot read property 'foo' of undefined\n" +
+          " at example (/tmp/example.coffee:2:3, <js>:5:20)";
       return parser.parseException(exc, this.callback);
     },
-    'it parses correctly': function(err, parsedObj) {
+    'it parses correctly': function (err, parsedObj) {
       assert.equal(parsedObj.frames[0].filename, "/tmp/example.coffee");
       assert.equal(parsedObj.frames[0].lineno, 2);
       assert.equal(parsedObj.frames[0].colno, 3);
@@ -84,9 +174,11 @@ var suite = vows.describe('parser').addBatch({
   'Multiple errors': {
     'Simple': {
       topic: function (err) {
-        var exc1 = new Error('First error');
-        var exc2 = new Error('Second error');
-        var containerExc = new Error();
+        var exc1, exc2, containerExc;
+
+        exc1 = new Error('First error');
+        exc2 = new Error('Second error');
+        containerExc = new Error();
         containerExc.errors = [exc1, exc2];
         return parser.parseException(containerExc, this.callback);
       },
@@ -96,9 +188,11 @@ var suite = vows.describe('parser').addBatch({
     },
     'exception.errors is an object': {
       topic: function (err) {
-        var exc1 = new Error('First error');
-        var exc2 = new Error('Second error');
-        var containerExc = new Error();
+        var exc1, exc2, containerExc;
+
+        exc1 = new Error('First error');
+        exc2 = new Error('Second error');
+        containerExc = new Error();
         containerExc.errors = {first: exc1, second: exc2};
         return parser.parseException(containerExc, this.callback);
       },
@@ -128,75 +222,3 @@ var suite = vows.describe('parser').addBatch({
     }
   }
 }).export(module, {error: false});
-
-
-function readJadeFixture(filename, includeJadeFilename, jadeLocals, callback) {
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      return callback(err);
-    } else {
-      var opts = {};
-      if (includeJadeFilename) {
-        opts.filename = __dirname + '/../fixture/jadeerr.jade';
-      }
-      var jadeFn = jade.compile(data, opts);
-      try {
-        jadeFn(jadeLocals);
-        return callback(new Error('expected this to break'));
-      } catch (e) {
-        return callback(null, e);
-      }
-    }
-  });
-}
-
-function parseAndVerifyException(jadeDebug) {
-  return {
-    topic: function(err, exc) {
-      parser.parseException(exc, this.callback);
-    },
-    'verify stack frames': function(err, parsedObj) {
-      assert.isNull(err);
-      assert.isObject(parsedObj);
-
-      var frames = parsedObj.frames;
-      assert.isArray(frames);
-
-      assert.equal(parsedObj.class, 'ReferenceError');
-      assert.equal(parsedObj.message, 'foo is not defined');
-
-      var i;
-      var numFrames = frames.length;
-      var cur;
-      assert.isTrue(numFrames >= 1);
-
-      var jadeDebugFound = false;
-      for (i = 0; i < numFrames; ++i) {
-        cur = frames[i];
-        assert.include(cur, 'method');
-        assert.include(cur, 'filename');
-        assert.include(cur, 'lineno');
-        if (cur.method !== '<jade>') {
-          assert.include(cur, 'colno');
-          assert.isNumber(cur.colno);
-        } else {
-          jadeDebugFound = true;
-        }
-
-        assert.isNumber(cur.lineno);
-        assert.isString(cur.method);
-        assert.isString(cur.filename);
-      }
-
-      if (jadeDebug) {
-        if (!jadeDebugFound) {
-          assert.isFalse('jade debug not found');
-        }
-      } else {
-        if (jadeDebugFound) {
-          assert.isFalse('jade debug found but should not be');
-        }
-      }
-    }
-  };
-};
