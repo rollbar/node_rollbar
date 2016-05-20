4f7256a3f2cab5c090ce18076ec0fdbae44b662b
Cory Virok <coryvirok@gmail.com>
2016-05-20 12:38:30 -0700

    Use rewire to fix uninitialize test
    The test was being run in the same process as the previous tests
    which meant that rollbar.init() had been called at least once.
    Since this test is meant to check the case where init() is not
    called at all, we need to use rewire to reset the state of the
    notifier module.
    
    In the future, notifiers will store their state in instance vars
    and not globally which will make this easier to test.
    
    @jondeandres

--- test/uninitialized.js
+++ test/uninitialized.js
@@ -1,15 +1,22 @@
 var util = require('util');
 var assert = require('assert');
 var vows = require('vows');
-var rollbar = require('../rollbar');
+var rewire = require('rewire');
 
 var ACCESS_TOKEN = '8802be7c990a4922beadaaefb6e0327b';
+
 
 
 var suite = vows.describe('rollbar.reportMessage').addBatch({
   'without init()': {
     topic: function() {
-      rollbar.reportMessage('hello world', 'error', null, this.callback);
+      var notifier = rewire('../lib/notifier');
+
+      var callback = this.callback;
+      notifier.__with__({initialized: false, apiClient: undefined})(function () {
+        notifier.handleError(new Error('hello world'), null, callback);
+      });
+
     },
 
     'should return a Rollbar uninitialized error': function(err) {
@@ -18,8 +25,12 @@ var suite = vows.describe('rollbar.reportMessage').addBatch({
 
     'now initialize and call reportMessage()': {
       topic: function() {
-        rollbar.init(ACCESS_TOKEN);
-        rollbar.reportMessage('hello world', 'error', null, this.callback);
+        var notifier = rewire('../lib/notifier');
+        var api = rewire('../lib/api');
+
+        api.init(ACCESS_TOKEN, {});
+        notifier.init(api, {});
+        notifier.handleError(new Error('hello world'), null, this.callback);
       },
 
       'verify no error is returned': function (err) {
