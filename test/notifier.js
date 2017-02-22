24ebc3b8878e7647f8aecc85dcb93740122e983b
Luke Ehresman <luke@ehresman.org>
2017-02-22 16:06:46 -0500

    Move pending request tracking up to notifier level
    This is done so it can be synchronous.  That is, so the pending item is
    added immediately when addItem is called.  On the API level, it is
    asynchronous, and thus not reliable enough to be absoltuely sure all
    pending requests are represented.

--- test/notifier.js
+++ test/notifier.js
@@ -33,13 +33,14 @@ var sandbox = null;
 vows.describe('notifier pending requests').addBatch({
   'pending requests': {
     topic: function() {
-      assert(api.pendingRequestCount() == 0);
+      assert(notifier.pendingItemsCount() == 0);
       sandbox = sinon.sandbox.create();
       sandbox.stub(https, 'request', function(){ /* do nothing */ });
       notifier.handleError(new Error('test'), this.callback);
+      assert(notifier.pendingItemsCount() == 1); // should have been enqueued synchronously
     },
     'it keeps track of pending requests': function() {
-      assert(api.pendingRequestCount() == 1);
+      assert(notifier.pendingItemsCount() == 0); // should have been dequeued before callback is called
       sandbox.restore();
     }
   }
